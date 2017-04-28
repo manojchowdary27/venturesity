9001127c2c2504a6730805d0697949b736d00917
Manoj Kumar.Pathipati <manoj27.rgukt@gmail.com>
2017-04-27 22:34:34 -0700

    my Last commit

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,20 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit60be42dc3eb457f6c2a3e18d6342e972
 {
+    public static $prefixesPsr0 = array (
+        'M' => 
+        array (
+            'Monolog' => 
+            array (
+                0 => __DIR__ . '/..' . '/monolog/monolog/src',
+            ),
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixesPsr0 = ComposerStaticInit60be42dc3eb457f6c2a3e18d6342e972::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
